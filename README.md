18836cc720655d57915088c9748c23c10d79e627
KorotchenkoSV <09z1p23k@mail.ru>
2025-11-05 08:21:06 +0700

    НОД games add

--- README.md
+++ README.md
@@ -4,5 +4,8 @@
 # Frontend Project 44
 [![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=KorotchenkoSV_frontend-project-44&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=KorotchenkoSV_frontend-project-44)
 
-# asciinema
-https://asciinema.org/a/LKPtqUfGR9QV2Qrsjn6s274AT
+## Демонстрация
+
+- [Аскинему игры «Четность»](https://asciinema.org/a/LKPtqUfGR9QV2Qrsjn6s274AT)
+- [Аскинему игры «Калькулятор»](https://asciinema.org/a/MYQGU6g4E77lbVV5jlm8dWV12)
+- [Аскинему игры «НОД»](https://asciinema.org/a/IP4qF8hA03t6eVIcYGWX3RkRu)
---
+++ src/games/calc.js
@@ -0,0 +1,37 @@
+import { runGame } from '../index.js';
+
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const generateQuestion = () => {
+  const num1 = getRandomInt(1, 50);
+  const num2 = getRandomInt(1, 50);
+  
+  // Случайная операция
+  const operations = ['+', '-', '*'];
+  const operation = operations[getRandomInt(0, operations.length - 1)];
+  
+  let correctAnswer;
+  switch (operation) {
+    case '+':
+      correctAnswer = num1 + num2;
+      break;
+    case '-':
+      correctAnswer = num1 - num2;
+      break;
+    case '*':
+      correctAnswer = num1 * num2;
+      break;
+    default:
+      correctAnswer = 0;
+  }
+  
+  const question = `${num1} ${operation} ${num2}`;
+  return { question, correctAnswer };
+};
+
+const calcGame = () => generateQuestion();
+
+export default () => {
+  const description = 'What is the result of the expression?';
+  runGame(calcGame, description);
+};
