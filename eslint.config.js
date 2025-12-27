cc948e8542f0455da61af041ef18fd01c73faa51
KorotchenkoSV <09z1p23k@mail.ru>
2025-12-27 17:22:16 +0700

    eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,15 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-import stylistic from "@stylistic/eslint-config"; // Добавляем импорт
+import { defineConfig } from 'eslint/config';
+import stylistic from '@stylistic/eslint-config';
 
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs}"],
+    files: ['**/*.{js,mjs,jsx}'],
     languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-      ecmaVersion: 2022,
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
-    plugins: { js },
-    extends: ["js/recommended"],
-    rules: {
-      ...stylistic.rules, // Добавляем правила Stylistic
-    },
+    ...stylistic, // Полностью заменяем конфигурацию на Stylistic
   },
-  {
-    files: ["**/*.cjs"],
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-      ecmaVersion: 2022,
-      sourceType: "commonjs",
-    },
-    plugins: { js },
-    extends: ["js/recommended"],
-  }
 ]);
 
